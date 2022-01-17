84587e0199b58bfcedca02f31315ed4a4bbe61d7
zenor0 <zenor0@outlook.con>
2022-01-17 22:39:39 +0800

    Update

--- C/daily_codes/test.c
+++ C/daily_codes/test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int FactorialEZ( const int N );
+
 int main(void)
 {
 	printf("this it a test");
@@ -7,8 +9,21 @@ int main(void)
 	int num;
 	scanf("%d", &num);
 
-
-	printf("You just input: %d", num);
+	FactorialEZ(num);
 
 	return 0;
+}
+
+int FactorialEZ( const int N )
+{
+	long long result = 1;
+	long tempVar = 1;
+	if (N < 0)
+		return 0;
+	for (int i = 0; i < N; i++)
+	{
+		result *= tempVar;
+		tempVar += 1;
+	}
+	return result;
 }
