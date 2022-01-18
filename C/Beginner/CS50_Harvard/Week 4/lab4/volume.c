3f6faec3ff3f11fc050cf623ba794163cf673236
zenor0 <zenor0@outlook.con>
2022-01-18 20:44:20 +0800

    Add lab4

--- C/Beginner/CS50_Harvard/Week 4/lab4/volume.c
+++ C/Beginner/CS50_Harvard/Week 4/lab4/volume.c
@@ -1,11 +1,17 @@
+// This source code is verified on CS50 IDE
+// BUT it doesn't work on my Windows PC ????
+// I don't know why, and I'm confused
 // Modifies the volume of an audio file
 
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// A char variable represents a byte data
 // Number of bytes in .wav header
+// Assume that all the input wav files are 16-bits signed sampled.
 const int HEADER_SIZE = 44;
+const int WAV_SAMPLE_SIZE = 16;
 
 int main(int argc, char *argv[])
 {
@@ -34,8 +40,17 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t wavHeader[HEADER_SIZE];
+    fread(&wavHeader, sizeof(wavHeader), 1, input);
+    fwrite(&wavHeader, sizeof(wavHeader), 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while (fread(&buffer, sizeof(buffer), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(buffer), 1, output);
+    }
 
     // Close files
     fclose(input);
